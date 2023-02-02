ea77d0e4156259f0bb56adf7f9d1090c20fd268b
Dmitry Mordanov <mordanov314@gmail.com>
2023-02-02 16:56:49 +0300

    middle2

--- client/main.py
+++ client/main.py
@@ -1,21 +1,41 @@
+# Клиент для простого TCP чата
+
 import socket
 import threading
 
-def read_sok():
-    while 1 :
-        data = sor.recv(1024)
-        print(data.decode('utf-8'))
+# Класс - поток, работающий на прием (пишет в консоль, все, что приходит от сервера)
+class CustomThread(threading.Thread):
+    def __init__(self, event):
+        # call the parent constructor
+        super(CustomThread, self).__init__()
+        # store the event
+        self.event = event
 
-server = '127.0.0.1', 50505  # Данные сервера
+    def run(self):
+        # Рабочий цикл потока, который работает на прием
+        while True:
+            if self.event.is_set():
+                break
+            data = sock.recv(1024)
+            print(data.decode('utf-8'))
+
+server = '127.0.0.1', 50505  # Подключаемся сюда
 alias = input("Input your alias: ") # Вводим наш псевдоним
-sor = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sor.bind(('', 0)) # Задаем сокет как клиент
-sor.sendto((alias+' Connect to server').encode('utf-8'), server)# Уведомляем сервер о подключении
-t = threading.Thread(target= read_sok)
-t.start()
-while 1 :
-    mensahe = input()
-    if mensahe == 'quit':
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.connect(server)
+sock.sendto(('[' + alias + ']Hello world!' ).encode('utf-8'), server) # Приветствие
+
+event = threading.Event()
+thread = CustomThread(event)
+thread.start()
+while True: # Рабочий цикл потока, который работает на отправку
+    inp = input()
+    if inp == 'quit':
         break
-    sor.sendto(('['+alias+']'+mensahe).encode('utf-8'), server)
+    sock.sendto(('[' + alias + ']' + inp).encode('utf-8'), server)
+
+# Гасим принимающий поток
+event.set()
+thread.join()
+
 print('Bye!')
--- server/main.py
+++ server/main.py
@@ -1,11 +1,13 @@
+# Сервер для простого TCP чата
+
 import socket
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind (('127.0.0.1',50505))
 clients = [] # Массив где храним адреса клиентов
 print ('Start Server')
-while 1 :
+while True:
     data , addres = sock.recvfrom(1024)
-    print (addres[0], addres[1])
+    print (addres[0], addres[1], ': ',data.decode('utf-8'))
     if  addres not in clients :
         clients.append(addres)# Если такого клиента нету , то добавить
         sock.sendto(b'Greeting!',clients[-1])
@@ -13,4 +15,3 @@ while 1 :
         if client == addres :
             continue # Не отправлять данные клиенту, который их прислал
         sock.sendto(data,client)
-
